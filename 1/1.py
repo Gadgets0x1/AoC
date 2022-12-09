3a50532dc05341171e5e1a30508c5fb8d6c87c06
Estefan.rm <estefa.rengifo@gmail.com>
2022-12-09 16:18:52 +0100

    1

--- 1/1.py
+++ 1/1.py
@@ -1,17 +1,15 @@
 with open('./elves', 'r') as f:
-    elves={}
-    calories=0
-    largestCalories=0
-    mostCalories={}
+    calories = 0
+    largestCalories = 0
+    mostCalories = {}
     count = 1
     for line in f:
         if(line.strip()):
-            calories=calories+int(line.strip())
-        else:
+            calories = calories+int(line.strip())
             if(calories > largestCalories):
                 largestCalories = calories
                 mostCalories["most"]=[count,calories]
-            elves[count] = calories
+        else:
             count=count+1
             calories = 0
     print(mostCalories)
