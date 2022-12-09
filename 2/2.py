23847e6f20021bd792dfb15d52f3865b34164d26
esren <estefa.rengifo@gmail.com>
2022-12-09 23:25:19 +0100

    2

---
+++ 2/2.py
@@ -0,0 +1,35 @@
+#(AX) Rock     1 (Z)Scissor 3 = 4
+#(BY) Paper    2 (X)Rock    1 = 3
+#(CZ) Scissors 3 (Y)Paper   2 = 5
+
+def gameRound(p1: str, p2: str) -> int:
+    if( (p1 == 'A' and p2 == 'Z')or
+        (p1 == 'B' and p2 == 'X')or
+        (p1 == 'C' and p2 == 'Y')):
+        return 0
+    if( (p1 == 'A' and p2 == 'Y')or
+        (p1 == 'B' and p2 == 'Z')or
+        (p1 == 'C' and p2 == 'X')):
+        return 6
+    return 3
+
+def shapeScore(shape: str) -> int: 
+    if(shape == "X"):
+        return 1
+    if(shape == "Y"):
+        return 2
+    if(shape == "Z"):
+        return 3
+
+def totalScore(shapeScore: int, outcomeScore: int) -> int:
+    total = shapeScore + outcomeScore
+    return total
+
+with open('./game.txt', 'r') as players:
+    players = players.read().splitlines()
+    t=0
+    for player in players:
+        playerOne = player[0]
+        playerTwo = player[2]
+        t = t + totalScore(gameRound(playerOne,playerTwo),shapeScore(playerTwo))
+    print(t)
